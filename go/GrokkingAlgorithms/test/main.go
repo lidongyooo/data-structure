ab0f15606bd693658fabd8e2f0ee0516aeae678f
lidongyooo <2271439559@qq.com>
2021-09-27 15:55:25 +0800

    commit

---
+++ go/GrokkingAlgorithms/test/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+func main() {
+	i := int(1)
+	fmt.Println(unsafe.Sizeof(i)) // 8
+	j := 1
+	fmt.Println(unsafe.Sizeof(j)) // 8
+	u := uint(1)
+	fmt.Println(unsafe.Sizeof(u)) // 8
+}
